d3776b1395a018e37e372f9e0e3fc3475e2694d7
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2020-01-10 15:47:07 +0100

    FIn exo modules

---
+++ exo-modules-service/src/main/java/fr/java/service/AppMain.java
@@ -0,0 +1,16 @@
+package fr.java.service;
+
+import java.util.stream.Stream;
+
+import fr.java.persistence.entities.UserEntity;
+import fr.java.persistence.repository.RepositoryFactory;
+import fr.java.persistence.repository.UserRepository;
+
+public class AppMain {
+	public static void main(String[] args) {
+		// UserRepositoryImpl is hidden
+		UserRepository userRepository = RepositoryFactory.createUser();
+		Stream<UserEntity> entities = userRepository.findAll();
+		entities.forEach(user -> System.out.println(user.getFirstName() + " " + user.getLastName()));
+	}
+}
